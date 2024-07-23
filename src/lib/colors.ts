de86a56c615cc55a666b95427088d7db4fde9105
adrivs <adrianvillalbasanchez@gmail.com>
2024-07-23 23:45:18 +0200

    Add more colors and small design changes

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -6,8 +6,75 @@ interface Color {
 
 export const colors: Color[] = [
     {
+        name: "Lake Placid Blue",
+        key: "lake-placid-blue",
+        hex: "#3A688F"
+    },
+
+    {
+        name: "Daphne Blue",
+        key: "daphne-blue",
+        hex: "#A0CED7"
+    },
+    {
+        name: "Sonic Blue",
+        key: "sonic-blue",
+        hex: "#B9D0DD"
+    },
+    {
+        name: "Shoreline Gold",
+        key: "shoreline-gold",
+        hex: "#9D8E84"
+    },
+    {
+        name: "Olympic White",
+        key: "olympic-white",
+        hex: "#E6ECE3"
+    },
+    {
+        name: "Burgundy Mist",
+        key: "burgundy-mist",
+        hex: "#976274"
+    },
+    {
+        name: "Black",
+        key: "black",
+        hex: "#212121"
+    },
+    {
+        name: "Sherwood Green",
+        key: "sherwood-green",
+        hex: "#246F6A"
+    },
+    {
         name: "Foam Green",
         key: "foam-green",
         hex: "#51B1AC"
+    },
+    {
+        name: "Surf Green",
+        key: "surf-green",
+        hex: "#A0D7C7"
+    },
+    {
+        name: "Inca Silver",
+        key: "inca-silver",
+        hex: "#9FA1A0"
+    },
+    {
+        name: "Candy Apple Red",
+        key: "candy-apple-red",
+        hex: "#7B231C"
+    },
+    {
+        name: "Dakota Red",
+        key: "dakota-red",
+        hex: "#9E2527"
+    },
+    {
+        name: "Fiesta Red",
+        key: "fiesta-red",
+        hex: "#BD4438"
     }
+
 ]
