037c3cc0587f17764f8fcef2691ce1c416f79347
adrivs <adrianvillalbasanchez@gmail.com>
2024-07-23 20:51:30 +0200

    Start rendering colors

---
+++ src/lib/colors.ts
@@ -0,0 +1,13 @@
+interface Color {
+    name: string;
+    key: string;
+    hex: string;
+}
+
+export const colors: Color[] = [
+    {
+        name: "Foam Green",
+        key: "foam-green",
+        hex: "#51B1AC"
+    }
+]
