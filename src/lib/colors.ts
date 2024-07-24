76d3244724fd69b1854f59a54863265521471896
adrivs <adrianvillalbasanchez@gmail.com>
2024-07-24 20:28:50 +0200

    Add more color formats and add dropdown to select format

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -2,79 +2,107 @@ interface Color {
     name: string;
     key: string;
     hex: string;
+    rgb: string;
+    hsl: string;
 }
 
 export const colors: Color[] = [
     {
         name: "Lake Placid Blue",
         key: "lake-placid-blue",
-        hex: "#3A688F"
+        hex: "#3A688F",
+        rgb: "rgb(58, 104, 143)",
+        hsl: "hsl(206, 42%, 39%)"
     },
-
     {
         name: "Daphne Blue",
         key: "daphne-blue",
-        hex: "#A0CED7"
+        hex: "#A0CED7",
+        rgb: "rgb(160, 206, 215)",
+        hsl: "hsl(190, 43%, 74%)"
     },
     {
         name: "Sonic Blue",
         key: "sonic-blue",
-        hex: "#B9D0DD"
+        hex: "#B9D0DD",
+        rgb: "rgb(185, 208, 221)",
+        hsl: "hsl(200, 36%, 80%)"
     },
     {
         name: "Shoreline Gold",
         key: "shoreline-gold",
-        hex: "#9D8E84"
+        hex: "#9D8E84",
+        rgb: "rgb(157, 142, 132)",
+        hsl: "hsl(27, 14%, 57%)"
     },
     {
         name: "Olympic White",
         key: "olympic-white",
-        hex: "#E6ECE3"
+        hex: "#E6ECE3",
+        rgb: "rgb(230, 236, 227)",
+        hsl: "hsl(90, 17%, 91%)"
     },
     {
         name: "Burgundy Mist",
         key: "burgundy-mist",
-        hex: "#976274"
+        hex: "#976274",
+        rgb: "rgb(151, 98, 116)",
+        hsl: "hsl(340, 21%, 49%)"
     },
     {
         name: "Black",
         key: "black",
-        hex: "#212121"
+        hex: "#212121",
+        rgb: "rgb(33, 33, 33)",
+        hsl: "hsl(0, 0%, 13%)"
     },
     {
         name: "Sherwood Green",
         key: "sherwood-green",
-        hex: "#246F6A"
+        hex: "#246F6A",
+        rgb: "rgb(36, 111, 106)",
+        hsl: "hsl(177, 51%, 29%)"
     },
     {
         name: "Foam Green",
         key: "foam-green",
-        hex: "#51B1AC"
+        hex: "#51B1AC",
+        rgb: "rgb(81, 177, 172)",
+        hsl: "hsl(177, 40%, 51%)"
     },
     {
         name: "Surf Green",
         key: "surf-green",
-        hex: "#A0D7C7"
+        hex: "#A0D7C7",
+        rgb: "rgb(160, 215, 199)",
+        hsl: "hsl(162, 41%, 74%)"
     },
     {
         name: "Inca Silver",
         key: "inca-silver",
-        hex: "#9FA1A0"
+        hex: "#9FA1A0",
+        rgb: "rgb(159, 161, 160)",
+        hsl: "hsl(150, 1%, 63%)"
     },
     {
         name: "Candy Apple Red",
         key: "candy-apple-red",
-        hex: "#7B231C"
+        hex: "#7B231C",
+        rgb: "rgb(123, 35, 28)",
+        hsl: "hsl(5, 62%, 30%)"
     },
     {
         name: "Dakota Red",
         key: "dakota-red",
-        hex: "#9E2527"
+        hex: "#9E2527",
+        rgb: "rgb(158, 37, 39)",
+        hsl: "hsl(359, 62%, 38%)"
     },
     {
         name: "Fiesta Red",
         key: "fiesta-red",
-        hex: "#BD4438"
+        hex: "#BD4438",
+        rgb: "rgb(189, 68, 56)",
+        hsl: "hsl(5, 56%, 48%)"
     }
-
-]
+];
